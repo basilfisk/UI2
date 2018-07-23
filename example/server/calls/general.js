f92eaf98ead7bd104437987d3388432b791404f0
Basil Fisk <bf@breato.co.uk>
2018-07-23 14:26:19 +0100

    Removed obsolete code

--- example/server/calls/general.js
+++ example/server/calls/general.js
@@ -37,7 +37,6 @@ class Calls {
 			// Check that only 1 user was returned
 			else {
 				if (data.length === 1) {
-//					login_group_users(session, data[0]);
 					// Read all users in the same company group as this user
 					that.mongoDB.db(that.admin.mongo.db).collection('va_user').find({"company":data[0].company,"group":data[0].group},{"username":1,"_id":-1}).toArray( function(err, users) {
 						var msg = {}, i, grps = [];
@@ -66,45 +65,6 @@ class Calls {
 				else {
 					msg = that.log('ADM022', []);
 					that.sendResponse(session, msg);
-				}
-			}
-		});
-	}
-	ZZZloginCheck (that, session, callback) {
-		var msg;
-		that.mongoDB.db(that.admin.mongo.db).collection('va_user').find({"username":session.params.username, "password":session.params.password},{"password":0}).toArray( (err, data) => {
-			// Error trying to retrieve data
-			if (err) {
-				msg = { code:'ADM018', prms:['user', err.message] }
-				callback(that, session, false, msg);
-			}
-			// Check that only 1 user was returned
-			else {
-				if (data.length === 1) {
-					// Read all users in the same company group as this user
-					that.mongoDB.db(that.admin.mongo.db).collection('va_user').find({"company":data[0].company,"group":data[0].group},{"username":1,"_id":-1}).toArray( (err, result) => {
-						var msg, i;
-						// Error trying to retrieve data
-						if (err) {
-							msg = { code:'ADM021', prms:['user', err.message] }
-							callback(that, session, false, msg);
-						}
-						// Return all user data
-						// Add user credentials from login query
-						// Add an array of users in this group
-						else {
-							msg = data[0];
-							msg.groupusers = [];
-							for (i=0; i<result.length; i++) {
-								msg.groupusers.push(result[i].username);
-							}
-							callback(that, session, true, msg);
-						}
-					});
-				}
-				else {
-					msg = { code:'ADM022', prms:[] }
-					callback(that, session, false, msg);
 				}
 			}
 		});
