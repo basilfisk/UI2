7827f474577458cd862f83e87f06a4939baaae2d
Basil Fisk <bf@breato.co.uk>
2018-07-31 09:04:04 +0100

    Support for callback on failure

--- example/client/support/common.js
+++ example/client/support/common.js
@@ -15,10 +15,11 @@ var common = {
 	 * @author Basil Fisk
 	 * @param {string} command Command to be run.
 	 * @param {object} json Object of parameters to the command in JSON format.
-	 * @param {function} callback Callback function to be run after data returned.
+	 * @param {function} success Callback function to be run after data returned.
+	 * @param {function} failure Callback function to be run after data returned.
 	 * @description Send a JWT protected command to the API then invoke a callback.
 	 */
-	apiCall: function (command, json, callback) {
+	apiCall: function (command, json, success, failure) {
 		var url, options;
 
 		url = system.api.host + ':' + system.api.port + '/1/' + command;
@@ -26,13 +27,12 @@ var common = {
 			type: 'GET',
 			dataType: 'json',
 			data: JSON.stringify(json),
-//			headers: { "Authorization": "Bearer " + me.jwt },	// THIS DOUBLES UP THE PROCESSING IN admin.js !!!!!!!!!
+//			headers: { "Authorization": "Bearer " + me.jwt },	// TODO THIS DOUBLES UP THE PROCESSING IN admin.js !!!!!!!!!
 			error: function (err) {
-				ui.messageBox('CMN001', [system.api.host, system.api.port, command]);
+				ui.messageBox('CMN001', [system.api.host, system.api.port, command], failure);
 			},
 			success: function (result) {
-//console.log(result);
-				callback('result', result);
+				success('result', result);
 			}
 		};
 
